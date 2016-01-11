aa5ec0485ccedcd7695bd5e6071f67efb976cfad
Mariano Gappa <mariano@movio.co>
2016-01-12 00:01:21 +1300

    Updated parser: much faster; describes Notation.

--- src/main/scala-2.11/OstinatoParserExampleApp.scala
+++ src/main/scala-2.11/OstinatoParserExampleApp.scala
@@ -22,29 +22,29 @@ object OstinatoParserExampleApp extends JSApp {
 
   @JSExport
   def refresh(input: html.Input, target: html.Div) = {
-    Notation.parseMatchString(input.value) match {
-      case Right(states: List[(ChessAction, ChessBoard)]) ⇒
+    val results = NotationParser.parseMatchString(input.value).results
+
+    results.head match {
+      case (steps, notationRules) ⇒
         currentPosition = -1
         render()
-        positions = states.map(_._2.toFen).toArray
+        positions = steps.filter(_._2.nonEmpty).map(_._2.get._2.toFen).toArray
 
         target.innerHTML = buttons +
-          states.zipWithIndex.grouped(2).zipWithIndex.map {
-            case (states: List[((ChessAction, ChessBoard), Int)], index: Int) ⇒
-              s"""<div class="line">${index + 1}. ${renderLine(states)}</div>"""
-          }.mkString
-
-      case Left(states: List[(String, Option[(ChessAction, ChessBoard)])]) ⇒
-        currentPosition = -1
-        render()
-        positions = states.filter(_._2.nonEmpty).map(_._2.get._2.toFen).toArray
-
-        target.innerHTML = buttons +
-          states.zipWithIndex.grouped(2).zipWithIndex.map {
+          steps.zipWithIndex.grouped(2).zipWithIndex.map {
             case (states: List[((String, Option[(ChessAction, ChessBoard)]), Int)], index: Int) ⇒
               s"""<div class="line">${index + 1}. ${renderLineWithErrors(states)}</div>"""
-          }.mkString
+          }.mkString + s"""<br/><div class="line" style="width: 100px">""" + showNotation(notationRules) + "</div>"
     }
+  }
+
+  private def showNotation(notationRules: Either[Option[NotationRules], NotationRules]) = notationRules match {
+    case Left(None) ⇒
+      "What is this?"
+    case Left(Some(r: NotationRules)) ⇒
+      "<b>Most likely:</b><br/> " + r.fullName
+    case Right(r: NotationRules) ⇒
+      "<b>Parsed as:</b><br/> " + r.fullName
   }
 
   @JSExport
@@ -71,16 +71,11 @@ object OstinatoParserExampleApp extends JSApp {
     }
   }
 
-  def renderLine(line: List[((ChessAction, ChessBoard), Int)]) = line.map {
-    case ((action: ChessAction, board: ChessBoard), halfIndex: Int) ⇒
-      s"""<a href="javascript:OstinatoParserExampleApp().currentPosition=$halfIndex; javascript:OstinatoParserExampleApp().render()">${action.toAn.toString}</a>"""
-  }.mkString(" ")
-
   def renderLineWithErrors(line: List[((String, Option[(ChessAction, ChessBoard)]), Int)]) = line.map {
     case ((rawAction: String, Some((action: ChessAction, board: ChessBoard))), halfIndex: Int) ⇒
-      s"""<a href="javascript:OstinatoParserExampleApp().currentPosition=$halfIndex; javascript:OstinatoParserExampleApp().render()">${action.toAn.toString}</a>"""
+      s"""<a href="javascript:OstinatoParserExampleApp().currentPosition=$halfIndex; javascript:OstinatoParserExampleApp().render()">${rawAction}</a>"""
     case ((rawAction: String, None), halfIndex: Int) ⇒
-      s"""<b>$rawAction</b>"""
+      s"""<i>$rawAction</i>"""
   }.mkString(" ")
 
 }
